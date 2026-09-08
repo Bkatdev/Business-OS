67783785401591314205bf256763b29aca9aa583
Ben Katzenberg <businessosdev@gmail.com>
2026-09-08 15:16:41 -0400

    Audit multiple businesses

--- website_auditor.py
+++ website_auditor.py
@@ -1,13 +1,17 @@
 from businesses import businesses
 
-business = businesses[0]
-
-print("Website Audit:", business["name"])
-if business["online_booking"] == False:
-    print("⚠ No online booking found")
-if business["website_chat"] == False:
-    print("⚠ No website chat found")
-if business["estimate_form"] == False:
-    print("⚠ No online estimate/request form found")
+for business in businesses:
     print("--------------------")
-print("Audit complete.")
+    print("Website Audit:", business["name"])
+    print("Location:", business["city"])
+
+    if business["online_booking"] == False:
+        print("⚠ No online booking found")
+
+    if business["website_chat"] == False:
+        print("⚠ No website chat found")
+
+    if business["estimate_form"] == False:
+        print("⚠ No online estimate/request form found")
+
+    print("Audit complete.")
