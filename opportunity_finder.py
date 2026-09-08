89ca943b6f02f3b287c8e4ef2417a192f823c020
Ben Katzenberg <businessosdev@gmail.com>
2026-09-08 14:58:37 -0400

    Define Opportunity Score v1

--- opportunity_finder.py
+++ opportunity_finder.py
@@ -4,23 +4,28 @@ business = {
     "website": "https://example.com",
     "reviews": 125,
     "online_booking": False,
-    "emergency_service": True
+    "emergency_service": True,
+    "website_chat": False,
+    "estimate_form": False
 }
 
 opportunity_score = 0
 
 if business["online_booking"] == False:
-    opportunity_score = opportunity_score + 25
+    opportunity_score += 25
 
 if business["emergency_service"] == True:
-    opportunity_score = opportunity_score + 15
+    opportunity_score += 20
 
-if business["online_booking"] == False:
-    print("Opportunity found: This business has no online booking.")
+if business["website_chat"] == False:
+    opportunity_score += 15
 
-print(business)
-print("Opportunity Score:", opportunity_score)
+if business["estimate_form"] == False:
+    opportunity_score += 20
 
+if business["reviews"] >= 100:
+    opportunity_score += 20
 
-
-
+print("Business:", business["name"])
+print("Location:", business["city"])
+print("Opportunity Score:", opportunity_score, "/ 100")
