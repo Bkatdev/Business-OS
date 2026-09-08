c02079c2b54bfe6480156df3845639c69697aee4
Ben Katzenberg <businessosdev@gmail.com>
2026-09-08 15:12:00 -0400

    Support multiple businesses in opportunity finder

--- opportunity_finder.py
+++ opportunity_finder.py
@@ -1,31 +1,52 @@
-business = {
-    "name": "Example Tree Service",
-    "city": "East Brunswick",
-    "website": "https://example.com",
-    "reviews": 125,
-    "online_booking": False,
-    "emergency_service": True,
-    "website_chat": False,
-    "estimate_form": False
-}
+businesses = [
+    {
+        "name": "Example Tree Service",
+        "city": "East Brunswick",
+        "reviews": 125,
+        "online_booking": False,
+        "emergency_service": True,
+        "website_chat": False,
+        "estimate_form": False
+    },
+    {
+        "name": "Example Landscaping",
+        "city": "Old Bridge",
+        "reviews": 65,
+        "online_booking": True,
+        "emergency_service": False,
+        "website_chat": False,
+        "estimate_form": True
+    },
+    {
+        "name": "Example Auto Repair",
+        "city": "Marlboro",
+        "reviews": 210,
+        "online_booking": False,
+        "emergency_service": False,
+        "website_chat": True,
+        "estimate_form": True
+    }
+]
 
-opportunity_score = 0
+for business in businesses:
+    opportunity_score = 0
 
-if business["online_booking"] == False:
-    opportunity_score += 25
+    if business["online_booking"] == False:
+        opportunity_score += 25
 
-if business["emergency_service"] == True:
-    opportunity_score += 20
+    if business["emergency_service"] == True:
+        opportunity_score += 20
 
-if business["website_chat"] == False:
-    opportunity_score += 15
+    if business["website_chat"] == False:
+        opportunity_score += 15
 
-if business["estimate_form"] == False:
-    opportunity_score += 20
+    if business["estimate_form"] == False:
+        opportunity_score += 20
 
-if business["reviews"] >= 100:
-    opportunity_score += 20
+    if business["reviews"] >= 100:
+        opportunity_score += 20
 
-print("Business:", business["name"])
-print("Location:", business["city"])
-print("Opportunity Score:", opportunity_score, "/ 100")
+    print("--------------------")
+    print("Business:", business["name"])
+    print("Location:", business["city"])
+    print("Opportunity Score:", opportunity_score, "/ 100")
